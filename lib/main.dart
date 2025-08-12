2302116078b19385adfda4d0430f16d209ba77ab
Shyunwook <gdnfl0117@gmail.com>
2025-08-13 02:05:14 +0900

    완성, 성능 최적화 작업 전

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/scheduler.dart';
 import 'package:mediapipe_channeling_test/camera_screen.dart';
 
 void main() {
-  WidgetsFlutterBinding.ensureInitialized();
-  // Enable hardware acceleration.
-  WidgetsBinding.instance.addPostFrameCallback((_) {
-    SchedulerBinding.instance.scheduleWarmUpFrame();
-  });
-
   runApp(const MainApp());
 }
 
